62be15606d6af81d44aa77522cc24589e18c970c
s20016 <s20016@std.it-college.ac.jp>
2023-08-26 21:29:30 +0900

    Add: Output items

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -4,8 +4,9 @@ export default function Main() {
   const [slug, setSlug] = useState("");
   const [name, setName] = useState("");
   const [items, setItems] = useState([]);
-  const [itemsCount, setItemsCount] = useState(0);
+  const [itemsCount, setItemsCount] = useState(1);
   const [defaultItems, setDefaultItems] = useState([
+    { slug: "news", name: "お知らせ" },
     { slug: "contact", name: "お問い合わせ" },
     { slug: "privacy", name: "プライバシーポリシー" },
     { slug: "site", name: "サイトマップ" },
@@ -13,32 +14,25 @@ export default function Main() {
 
   const [headerItem, setHeaderItem] = useState("header__item");
   const [headerLink, setHeaderLink] = useState("header__item-link");
-  const [headerAnchor, setHeaderAnchor] = useState("--active");
+  const [headerActive, setHeaderActive] = useState("header__item--active");
 
-  const generatedList = [defaultItems.map((items) => {
-    
-  })];
-
-  console.log(generatedList.length);
-
-  function handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
     setDefaultItems((currentItems) => {
       return [
-        ...currentItems.slice(itemsCount, itemsCount + 1),
+        ...currentItems.slice(0, itemsCount),
         {
           slug: slug,
           name: name,
         },
-        ...currentItems.slice(itemsCount + 2),
+        ...currentItems.slice(itemsCount),
       ];
     });
 
     setSlug("");
     setName("");
     setItemsCount(itemsCount + 1);
-    console.log(itemsCount);
-  }
+  };
 
   return (
     <>
@@ -46,45 +40,82 @@ export default function Main() {
         <div className="input__wrapper">
           <form className="input__form" onSubmit={handleSubmit}>
             <div className="input__form-item">
-              <label htmlFor="slug-item">slug</label>
+              <label htmlFor="slug">slug</label>
               <input
                 type="text"
-                id="slug-item"
+                id="slug"
                 placeholder="news"
                 value={slug}
                 onChange={(e) => setSlug(e.target.value)}
               />
             </div>
             <div className="input__form-item">
-              <label htmlFor="name-item">name</label>
+              <label htmlFor="name">name</label>
               <input
                 type="text"
-                id="name-item"
+                id="name"
                 placeholder="お知らせ"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
             </div>
-            <button>Submit</button>
+            <button className="input__button" disabled={!(slug && name)}>
+              Generate Link
+            </button>
           </form>
         </div>
       </section>
 
-      {/* <section className="output cmn-py">
+      <section className="output cmn-py">
         <div className="output__wrapper">
           <p className="output__title">Header</p>
+          <form className="input__form input__form--setting">
+            <div className="input__form-item">
+              <label htmlFor="headerItem">&lt;li&gt; classname</label>
+              <input
+                type="text"
+                id="headerItem"
+                placeholder={headerItem}
+                // value={headerItem}
+                onChange={(e) => setHeaderItem(e.target.value)}
+              />
+            </div>
+            <div className="input__form-item">
+              <label htmlFor="headerItem">&lt;a&gt; classname</label>
+              <input
+                type="text"
+                id="headerLink"
+                placeholder={headerLink}
+                // value={headerLink}
+                onChange={(e) => setHeaderLink(e.target.value)}
+              />
+            </div>
+            <div className="input__form-item">
+              <label htmlFor="headerItem">active classname</label>
+              <input
+                type="text"
+                id="headerActive"
+                placeholder={headerActive}
+                // value={headerActive}
+                onChange={(e) => setHeaderActive(e.target.value)}
+              />
+            </div>
+          </form>
           <ul className="output__list">
-            {defaultItems.map((item) => {
-              const uniqueID = crypto.randomUUID
+            {defaultItems.map((item, index) => {
+              const newID = crypto.randomUUID();
               return (
-                <li className="output__item" id={uniqueID} key={uniqueID}>
-                  {item.slug}, {item.name}
+                <li className="output__item" key={newID}>
+                  &lt;li class="{headerItem} &lt;?php get_meta('slug') === '
+                  {item.slug}' ? '{headerActive}' : ''?&gt;
+                  "&gt;&lt;a class="{headerLink}" href="/{item.slug}/"&gt;
+                  {item.name}&lt;/a&gt;&lt;/li&gt;
                 </li>
               );
             })}
           </ul>
         </div>
-      </section> */}
+      </section>
     </>
   );
 }

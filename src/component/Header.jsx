45977dfab5d2d411458320083589639a62c9db57
JC TINIO <j_tinio@sfidax.jp>
2023-09-19 11:35:40 +0900

    Fix: link

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,8 +1,10 @@
 export default function Header() {
+  const currentPage = window.location.href;
+
   return (
     <header className="header">
       <div className="header__wrapper">
-        <a className="header__logo" href="/">
+        <a className="header__logo" href={currentPage}>
           <h1 className="header__title">NAVIZEN</h1>
         </a>
         {/* <ul className="header__list">

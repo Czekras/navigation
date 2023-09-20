46eca49ce60318b18f1a451a825a81790d7ed6e5
JC TINIO <j_tinio@sfidax.jp>
2023-09-20 10:49:21 +0900

    Fix: Favivon, CSS

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,20 +5,8 @@ export default function Header() {
     <header className="header">
       <div className="header__wrapper">
         <a className="header__logo" href={currentPage}>
-          <h1 className="header__title">NAVIZEN</h1>
+          <h1 className="header__title">NAVI<span>ZEN</span></h1>
         </a>
-        {/* <ul className="header__list">
-          <li className="heade__item">
-            <a className="header__link" href="">
-              <span className="material-symbols-outlined">code</span>
-            </a>
-          </li>
-          <li className="heade__item">
-            <a className="header__link" href="">
-              <span className="material-symbols-outlined">dark_mode</span>
-            </a>
-          </li>
-        </ul> */}
       </div>
     </header>
   );
